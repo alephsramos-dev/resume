4b457ee5ccab25bc8839c00929f00c2229f85887
Aleph Ramos <alephsramosdev@gmail.com>
2025-05-30 17:25:20 -0300

    commit rápido

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './components/Header'
-import './styles/reset.css'
+import Header from '@/components/Header'
+import '@/styles/reset.css'
 
 export default function App() {
   return (
