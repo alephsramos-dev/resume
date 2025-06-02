4d4b8cce90b28cc21adf51dd16333d05198ac44d
Aleph Ramos <alephsramosdev@gmail.com>
2025-06-02 13:42:48 -0300

    'add'

--- src/Data/techIcons.jsx
+++ src/Data/techIcons.jsx
@@ -1,22 +1,39 @@
-import { SiJavascript, SiHtml5, SiCss3, SiTypescript, SiNextdotjs, SiStyledcomponents, SiVite, SiRedux, SiNodedotjs, SiJest, SiMongodb, SiExpress, SiSass, SiTailwindcss, SiBootstrap, SiFigma, SiGit, SiGithub, SiVercel, SiNetlify, SiFirebase } from "react-icons/si";
+import reactIcon from '@/assets/icons/reactjs.svg';
+import javascriptIcon from '@/assets/icons/javascript.svg';
+import htmlIcon from '@/assets/icons/html.svg';
+import cssIcon from '@/assets/icons/css.svg';
+import typescriptIcon from '@/assets/icons/typescript.svg';
+import viteIcon from '@/assets/icons/vite.svg';
+import nextjsIcon from '@/assets/icons/nextjs.svg';
+import reduxIcon from '@/assets/icons/redux.svg';
+import nodejsIcon from '@/assets/icons/nodejs.svg';
+import jestIcon from '@/assets/icons/jest.svg';
+import mongodbIcon from '@/assets/icons/mongodb.svg';
+import expressIcon from '@/assets/icons/express.svg';
+import sassIcon from '@/assets/icons/sass.svg';
+import figmaIcon from '@/assets/icons/figma.svg';
+import gitIcon from '@/assets/icons/git.svg';
+import githubIcon from '@/assets/icons/github.svg';
+import vercelIcon from '@/assets/icons/vercel.svg';
+import firebaseIcon from '@/assets/icons/firebase.svg';
 
 export const techIcons = {
-  react: <img src={import.meta.env.BASE_URL + '@/assets/icons/reactjs.svg'} alt="React" style={{width: 25, height: 25}} />,
-  javascript: <img src={import.meta.env.BASE_URL + '@/assets/icons/javascript.svg'} alt="JavaScript" style={{width: 25, height: 25}} />,
-  html: <img src={import.meta.env.BASE_URL + '@/assets/icons/html.svg'} alt="HTML5" style={{width: 25, height: 25}} />,
-  css: <img src={import.meta.env.BASE_URL + '@/assets/icons/css.svg'} alt="CSS3" style={{width: 25, height: 25}} />,
-  typescript: <img src={import.meta.env.BASE_URL + '@/assets/icons/typescript.svg'} alt="TypeScript" style={{width: 25, height: 25}} />,
-  vite: <img src={import.meta.env.BASE_URL + '@/assets/icons/vite.svg'} alt="Vite" style={{width: 25, height: 25}} />,
-  nextjs: <img src={import.meta.env.BASE_URL + '@/assets/icons/nextjs.svg'} alt="Next.js" style={{width: 25, height: 25}} />,
-  redux: <img src={import.meta.env.BASE_URL + '@/assets/icons/redux.svg'} alt="Redux" style={{width: 25, height: 25}} />,
-  nodejs: <img src={import.meta.env.BASE_URL + '@/assets/icons/nodejs.svg'} alt="Node.js" style={{width: 25, height: 25}} />,
-  jest: <img src={import.meta.env.BASE_URL + '@/assets/icons/jest.svg'} alt="Jest" style={{width: 25, height: 25}} />,
-  mongodb: <img src={import.meta.env.BASE_URL + '@/assets/icons/mongodb.svg'} alt="MongoDB" style={{width: 25, height: 25}} />,
-  express: <img src={import.meta.env.BASE_URL + '@/assets/icons/express.svg'} alt="Express" style={{width: 25, height: 25}} />,
-  sass: <img src={import.meta.env.BASE_URL + '@/assets/icons/sass.svg'} alt="Sass" style={{width: 25, height: 25}} />,
-  figma: <img src={import.meta.env.BASE_URL + '@/assets/icons/figma.svg'} alt="Figma" style={{width: 25, height: 25}} />,
-  git: <img src={import.meta.env.BASE_URL + '@/assets/icons/git.svg'} alt="Git" style={{width: 25, height: 25}} />,
-  github: <img src={import.meta.env.BASE_URL + '@/assets/icons/github.svg'} alt="GitHub" style={{width: 25, height: 25}} />,
-  vercel: <img src={import.meta.env.BASE_URL + '@/assets/icons/vercel.svg'} alt="Vercel" style={{width: 25, height: 25}} />,
-  firebase: <img src={import.meta.env.BASE_URL + '@/assets/icons/firebase.svg'} alt="Firebase" style={{width: 25, height: 25}} />,
+  react: <img src={reactIcon} alt="React" style={{width: 25, height: 25}} />,
+  javascript: <img src={javascriptIcon} alt="JavaScript" style={{width: 25, height: 25}} />,
+  html: <img src={htmlIcon} alt="HTML5" style={{width: 25, height: 25}} />,
+  css: <img src={cssIcon} alt="CSS3" style={{width: 25, height: 25}} />,
+  typescript: <img src={typescriptIcon} alt="TypeScript" style={{width: 25, height: 25}} />,
+  vite: <img src={viteIcon} alt="Vite" style={{width: 25, height: 25}} />,
+  nextjs: <img src={nextjsIcon} alt="Next.js" style={{width: 25, height: 25}} />,
+  redux: <img src={reduxIcon} alt="Redux" style={{width: 25, height: 25}} />,
+  nodejs: <img src={nodejsIcon} alt="Node.js" style={{width: 25, height: 25}} />,
+  jest: <img src={jestIcon} alt="Jest" style={{width: 25, height: 25}} />,
+  mongodb: <img src={mongodbIcon} alt="MongoDB" style={{width: 25, height: 25}} />,
+  express: <img src={expressIcon} alt="Express" style={{width: 25, height: 25}} />,
+  sass: <img src={sassIcon} alt="Sass" style={{width: 25, height: 25}} />,
+  figma: <img src={figmaIcon} alt="Figma" style={{width: 25, height: 25}} />,
+  git: <img src={gitIcon} alt="Git" style={{width: 25, height: 25}} />,
+  github: <img src={githubIcon} alt="GitHub" style={{width: 25, height: 25}} />,
+  vercel: <img src={vercelIcon} alt="Vercel" style={{width: 25, height: 25}} />,
+  firebase: <img src={firebaseIcon} alt="Firebase" style={{width: 25, height: 25}} />,
 };
