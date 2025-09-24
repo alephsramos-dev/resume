d1250d21a0629381c037c8cf321ffab36f7548d2
alephsramos <alephsramosdev@gmail.com>
2025-09-24 20:28:00 -0300

    'add'

--- src/pages/Projects/All.jsx
+++ src/pages/Projects/All.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import Header from "./Header";
 import styled from "styled-components";
 import Portfolio from "./Portfolio";
-import DarkVeil from "@/components/ui/Patterns/DarkVeil.jsx";
-
 
 const Container = styled.div`
     width: 100%;
@@ -44,12 +42,12 @@ export default function All() {
         <>
             <Container>
                 <Bg>
-                    <DarkVeil
+                    {/* <DarkVeil
                         speed={0.6}
                         hueShift={330}
                         noiseIntensity={0.1}
                         warpAmount={2}
-                    />
+                    /> */}
                 </Bg>
                 <Header /> 
                     <Portfolio />
