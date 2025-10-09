3753915ea836d576267726c4ac1e60be3475d232
alephsramos <alephsramosdev@gmail.com>
2025-10-08 21:07:50 -0300

    'add'

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 
 // Layout Components
 import Header from '@/components/layout/Header'
@@ -11,9 +11,25 @@ import Curriculo from '@/pages/Resume/All'
 import Project from '@/pages/Projects/All'
 import ProjectDetails from './pages/Projects/ProjectDetails'
 
+// ScrollToTop Component
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'instant' // ou 'smooth' para um scroll suave
+    });
+  }, [pathname]);
+
+  return null;
+}
+
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path="/" element={<All />} />
